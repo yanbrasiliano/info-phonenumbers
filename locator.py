d2591d63ef5683f8a4593a1396442a0e14d8bdf4
yanbrasiliano <yanpenabr@gmail.com>
2021-02-02 23:14:33 -0300

    Update locator.py

--- locator.py
+++ locator.py
@@ -2,14 +2,14 @@ import phonenumbers
 from phonenumbers import geocoder, carrier
 
 # Enter your number with country code and ddd #
-phoneNumer = phonenumbers.parse("+5551999999999")
+phoneNumer = phonenumbers.parse("+55xxxxxxxxxxx") # -> enter a number here.
 
 # capture provider #
-Carrier = carrier.name_for_number(phoneNumer, 'pt-br')
+carrier = carrier.name_for_number(phoneNumer, 'pt-br')
 
 # capture region #
-Region = geocoder.description_for_number(phoneNumer, 'pt-br')
+region = geocoder.description_for_number(phoneNumer, 'pt-br')
 
 # Mostra resultados #
 print(f"PROVIDER: {carrier}.")
-print(f"STATE UF: {Region}.")
+print(f"STATE UF: {region}.")
